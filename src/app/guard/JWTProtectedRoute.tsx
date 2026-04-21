a35e5a36e5a98b82c917f2db76339fe5a9223ffc
UlisesChoco <ulisesjustosaucedo@gmail.com>
2026-04-21 19:02:01 +0000

    [REC-48] Fix bug where User can browse pages with a expired JWT

--- src/app/guard/JWTProtectedRoute.tsx
+++ src/app/guard/JWTProtectedRoute.tsx
@@ -1,11 +1,24 @@
 import { Navigate, Outlet } from "react-router-dom";
-import { getAuthToken } from "../../features/auth/service/auth-token.storage";
+import { clearAuthToken, getAuthToken } from "../../features/auth/service/auth-token.storage";
 
 export function JWTProtectedRoute() {
     const token = getAuthToken();
 
     if (!token)
         return <Navigate to="/login" replace />;
+
+    try {
+        const payload = JSON.parse(atob(token.split(".")[1]));
+        const currentTime = Math.floor(Date.now() / 1000);
+
+        if (payload.exp < currentTime) {
+            clearAuthToken();
+            return <Navigate to="/login" replace />;
+        }
+    } catch (error) {
+        clearAuthToken();
+        return <Navigate to="/login" replace />;
+    }
 
     return <Outlet />;
 }
